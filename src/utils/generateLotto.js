183950618611d322d468f692c876e5ecc66d6bfe
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-30 18:18:19 +0900

    feat(generateLotto): 로또 발행 숫자 오름차순으로 저장

--- src/utils/generateLotto.js
+++ src/utils/generateLotto.js
@@ -2,5 +2,6 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 export function generateLotto() {
   const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  lottoNumbers.sort((a, b) => a - b);
   return lottoNumbers;
 }
