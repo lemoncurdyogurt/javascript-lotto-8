44897552601950d5f2fbb097ac075fd7b4a7fb18
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-30 15:20:36 +0900

    feat(InputView): 사용자 입력값을 받는 기능 구현

---
+++ src/view/InputView.js
@@ -0,0 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+import { parseLottoNumbers } from "../utils/parseLottoNumbers";
+
+class InputView {
+  async purchaseInput() {
+    const purchaseAmount = Number(
+      await Console.readLineAsync("구입금액을 입력해 주세요.\n")
+    );
+    return purchaseAmount;
+  }
+
+  async lottoNumbersInput() {
+    const numbersInput =
+      await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+    const lottoNumbers = parseLottoNumbers(numbersInput);
+    return lottoNumbers;
+  }
+
+  async bonusNumberInput() {
+    const bonusNumber = Number(
+      await Console.readLineAsync("보너스 번호를 입력해 주세요.\n")
+    );
+    return bonusNumber;
+  }
+}
+
+export default InputView;
