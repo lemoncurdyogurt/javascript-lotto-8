1ce62a368fadc2437912d54f6f6fcdeb8f88b4e3
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-03 13:15:43 +0900

    refactor: 로또 특화 규칙 에러 메세지 상수화

---
+++ src/constants/lottoMessages.js
@@ -0,0 +1,5 @@
+export const LOTTO_ERROR = {
+  INVALID_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
+  DUPLICATE_NUMBER: "[ERROR] 로또 번호에 중복된 숫자가 있습니다.",
+  INVALID_RANGE: "[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.",
+};
