e84d084c0abf05a6a60ffda6fc118cd8274c30fa
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-03 15:10:46 +0900

    feat: 로또 유효성 검사 중 숫자타입 검사 추가

--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -10,19 +10,28 @@ class Lotto {
   }
 
   #validate(numbers) {
+    // 길이 검사
     if (numbers.length !== LOTTO_CONSTANTS.NUMBER_LENGTH) {
       throw new Error(LOTTO_ERROR.INVALID_LENGTH);
     }
+    // 중복 검사
     const hasDuplicates = new Set(numbers).size !== numbers.length;
     if (hasDuplicates) {
       throw new Error(LOTTO_ERROR.DUPLICATE_NUMBER);
     }
-
+    // 범위 검사
     const isOutOfRange = numbers.some(
       (n) => n < LOTTO_CONSTANTS.MIN_NUMBER || n > LOTTO_CONSTANTS.MAX_NUMBER,
     );
     if (isOutOfRange) {
       throw new Error(LOTTO_ERROR.INVALID_RANGE);
+    }
+    // 숫자 검사
+    const hasInvalidType = numbers.some(
+      (n) => typeof n !== "number" || Number.isNaN(n),
+    );
+    if (hasInvalidType) {
+      throw new Error(LOTTO_ERROR.INVALID_TYPE);
     }
   }
   getNumbers() {
